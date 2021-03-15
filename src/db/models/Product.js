679f1dd90433bd1d43b3bf794b23eac19f1d4e6c
halanzi <homoudnan@gmail.com>
2021-03-15 18:43:03 +0300

    Express Feature 6 complete

---
+++ src/db/models/Product.js
@@ -0,0 +1,36 @@
+// Slug
+const SequelizeSlugify = require("sequelize-slugify");
+
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define("Product", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    slug: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      defaultValue: 5,
+      validate: {
+        min: 1,
+      },
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  SequelizeSlugify.slugifyModel(Product, {
+    source: ["name"],
+  });
+
+  return Product;
+};
